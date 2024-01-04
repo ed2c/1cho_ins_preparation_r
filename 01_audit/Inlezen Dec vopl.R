2b6372f16e484237a10fc5d0e9bb8d93cd76a56a
Corneel <corneel_den_hartogh@users.noreply.github.com>
2024-01-04 18:00:35 +0100

    audit and run dec vopl

--- 01_audit/Inlezen Dec vopl.R
+++ 01_audit/Inlezen Dec vopl.R
@@ -25,26 +25,11 @@
 
 ## Lees alle benodigde bestanden in:
 Bestandspad <- paste0(
-  Sys.getenv("NETWORK_DIR"),
-  "Datasets/1cHO/2022/referentietabellen en documentatie/Dec_vopl.asc"
+  config::get("metadata_1cho_decoding_files_dir"), "Dec_vopl.asc"
 )
 
-Dec_vopl <- read_delim(Bestandspad,
-  col_types = cols(X1 = col_character()),
-  delim = ";",
-  col_names = FALSE,
-  locale = locale(encoding = "windows-1252")
-)
-## xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-## Up to date check
-## xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-up_to_date(
-  bestandspad = Bestandspad,
-  frequentie = 365,
-  contact = "Helmut Matheis",
-  inleesscript = "Dec vooropl.R"
-)
+Dec_vopl <- read_fwf(Bestandspad,
+                     fwf_widths(c(5, 200)))
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## 2. BEWERKEN ####
@@ -53,29 +38,24 @@ up_to_date(
 ## Splits kolom X1 in 2 kolommen en geef de juiste kolomnamen
 Dec_vopl <- Dec_vopl %>%
   ## Verwijderen van accenten
-  mutate_at(
-    c("X1"),
+  mutate(across(
+    c("X1", "X2"),
     ~ stringi:::stri_trans_general(str = ., id = "Latin-ASCII")
-  ) %>%
+  )) %>%
   ## Splits kolom X1 in 2 kolommen
-  mutate(
-    INS_Hoogste_vooropleiding_binnen_HO_code = substr(X1, 1, 5),
-    INS_Hoogste_vooropleiding_binnen_HO_naam = trimws(substr(X1, 6, 200))
-  ) %>%
-  ## Verwijder kolom X1
-  select(-X1)
-
-
-## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  rename(
+    from = X1,
+    to = X2
+  )
 
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## BEWAAR & RUIM OP ####
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# Bewaar bestand in de versies die als mapping-tables gebruikt worden
-# saverds_csv(Dec_vooropl, "Mapping_INS_Hoogste_vooropleiding_binnen_HO_code_INS_Hoogste_vooropleiding_binnen_HO_naam", save_csv = T)
-
-vvmover::write_file_proj(Dec_vopl, "INS_Dec_vopl")
+write_file_proj(Dec_vopl,
+                name = "Mapping_INS_Vooropleiding_code_INS_Vooropleiding_soort",
+                full_dir = Sys.getenv("MAP_TABLE_DIR"),
+                extensions = "csv")
 
 clear_script_objects()
