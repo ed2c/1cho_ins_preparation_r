6aacc64a1a2073c0cb9e888454049b084bcc7471
Tomeriko96 <IwanTomer@hotmail.com>
2024-06-11 13:59:48 +0200

    feat: use script validation when build is run (#4)
    Co-authored-by: tin900 <t.iwan@vu.nl>

--- build.R
+++ build.R
@@ -13,5 +13,16 @@ sScripts_to_run <- sFolders %>%
    map(., ~paste0(., "/", list.files(.))) %>%
    unlist()
 
+# Create a data frame for script validation
+dfScriptvalidation <- tribble(
+  ~script,
+  sScripts_to_run
+) %>%
+  unnest(c(script))
 
-walk(sScripts_to_run, source)
+## Read the Script validation data in
+##' *Note*: All scripts are run here, it takes a long time
+dfScriptvalidation <- dfScriptvalidation %>%
+  mutate(validation_df = map_dfr(script, safely(~ vusa::validate_script_proj(.x, TRUE)))) %>%
+  unnest_wider(validation_df) %>%
+  unnest_wider(result)
